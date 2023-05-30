aee183e009105e23dc543e6d83a7aaaf59d33583
n1f1 <84967079+n1f1@users.noreply.github.com>
2023-05-30 13:29:22 +0300

    Update NetworkConstants.cs

--- Networking/Common/NetworkConstants.cs
+++ Networking/Common/NetworkConstants.cs
@@ -1,10 +1,11 @@
 ﻿namespace Networking.Common
 {
+    //TODO: remove constants and calculate RTT at runtime 
     public static class NetworkConstants
     {
-        public const int BaseLatency = 300;
+        public const int BaseLatency = 100;
         public const int JitterDelta = 0;
-        public static float RTT => 0.3f;
+        public static float RTT => 0.1f;
         public static float ServerFixedDeltaTime => 0.1f;
     }
 }
